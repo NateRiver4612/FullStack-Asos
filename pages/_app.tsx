ca7119dcf1a7993cf91b7fa24fce65cfbfad4bcc
Nguyen Chi Nhan <519h0127@student.tdtu.edu.vn>
2022-12-03 18:33:49 +0700

    Fix bug for deploy

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -8,7 +8,7 @@ import { useEffect, useState } from "react";
 import Spinner from "../components/progress-bar/progress-bar.component";
 
 import { navigationData, footerData } from "../public/data";
-import PaymentSection from "../components/payment/payment-section.component";
+import PaymentSection from "../components/payment/Payment-Section.component";
 // import Footer from "../components/footer/Footer.component";
 
 import "nprogress/nprogress.css";
@@ -29,7 +29,7 @@ const Navigation = dynamic(
   }
 );
 
-const Footer = dynamic(() => import("../components/footer/footer.component"), {
+const Footer = dynamic(() => import("../components/footer/Footer.component"), {
   ssr: false,
 });
 
