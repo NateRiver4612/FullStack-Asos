079c23121fc278cadc144feb6ae217d587e56947
Nguyen Chi Nhan <519h0127@student.tdtu.edu.vn>
2022-12-04 00:24:09 +0700

    Fix bug for deploy

--- pages/[mainRouteId]/[categoryRouteId]/[categoryId]/index.js
+++ pages/[mainRouteId]/[categoryRouteId]/[categoryId]/index.js
@@ -80,14 +80,18 @@ const ProductList = ({ data }) => {
 };
 
 export async function getServerSideProps({ params: { categoryId } }) {
-  const response = await fetch("http://localhost:3000/api/listProductData", {
-    body: JSON.stringify({ categoryId: categoryId }),
-    method: "POST",
-  });
+  try {
+    const response = await fetch("http://localhost:3000/api/listProductData", {
+      body: JSON.stringify({ categoryId: categoryId }),
+      method: "POST",
+    });
 
-  const data = await response.json();
+    const data = await response.json();
 
-  return { props: { data: data } };
+    return { props: { data: data } };
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 export default ProductList;
