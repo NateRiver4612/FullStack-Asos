7fe98209fbc7b20e5b5d6f99bde5dadfddf81610
NateRiver4612.github.io <nguyenchinhan502@gmail.com>
2023-01-19 20:21:04 +0700

    Fix image buh

--- next.config.js
+++ next.config.js
@@ -4,6 +4,7 @@ const nextConfig = {
   swcMinify: true,
   trailingSlash: true,
   images: {
+    unoptimized: true,
     domains: [
       "static.wikia.nocookie.net",
       "lh3.googleusercontent.com",
