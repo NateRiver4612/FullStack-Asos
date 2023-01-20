8f5432ecee1af60b6e66f6e2744edaae376e9a21
NateRiver4612.github.io <nguyenchinhan502@gmail.com>
2023-01-20 13:24:12 +0700

    Update spinner css

--- components/spinner/spinner.component.tsx
+++ components/spinner/spinner.component.tsx
@@ -2,9 +2,9 @@ import React from "react";
 
 const Spinner = () => {
   return (
-    <div className="h-screen w-screen top-0 bg-gray-200/60 overflow-hidden bottom-0 z-40 fixed flex justify-center items-center">
+    <div className="h-screen w-screen top-0 bg-gray-400/80 overflow-hidden bottom-0 z-40 fixed flex justify-center items-center">
       <div
-        className="spinner-border text-gray-400 z-50 animate-spin inline-block w-10 h-10 border-4 rounded-full"
+        className="spinner-border text-gray-300 z-50 animate-spin inline-block w-10 h-10 border-4 rounded-full"
         role="status"
       >
         <span className="visually-hidden">Loading...</span>
