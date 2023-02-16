f90a231b5615e4535a7aa250f8bc71c3a972e74a
NateRiver4612.github.io <nguyenchinhan502@gmail.com>
2023-02-16 23:59:54 +0700

    Update icon of google and facebook login

--- components/register/form.component.tsx
+++ components/register/form.component.tsx
@@ -179,17 +179,42 @@ const RegisterForm = ({ switchState, setSwitchState, setLoading }) => {
             <span className="h-[1x] w-full border-b-[1px] border-gray-400"></span>
           </div>
           <div className="w-full flex gap-3 cursor-pointer justify-center">
-            <Image
+            <button
               onClick={handleGoogleSignIn}
-              src="/images/google_icon.png"
-              height={25}
-              width={25}
-            ></Image>
-            <Image
-              src="/images/facebook_icon.jpg"
-              height={25}
-              width={45}
-            ></Image>
+              type="button"
+              data-mdb-ripple="true"
+              data-mdb-ripple-color="light"
+              className="inline-block bg-red-600 rounded-full p-2 mb-2 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:shadow-lg focus:shadow-lg focus:outline-none focus:ring-0 active:shadow-lg transition duration-150 ease-in-out"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 488 512"
+                className="w-4 h-4 text-white "
+              >
+                <path
+                  fill="currentColor"
+                  d="M488 261.8C488 403.3 391.1 504 248 504 110.8 504 0 393.2 0 256S110.8 8 248 8c66.8 0 123 24.5 166.3 64.9l-67.5 64.9C258.5 52.6 94.3 116.6 94.3 256c0 86.5 69.1 156.6 153.7 156.6 98.2 0 135-70.4 140.8-106.9H248v-85.3h236.1c2.3 12.7 3.9 24.9 3.9 41.4z"
+                />
+              </svg>
+            </button>
+            <button
+              onClick={handleGoogleSignIn}
+              type="button"
+              data-mdb-ripple="true"
+              data-mdb-ripple-color="light"
+              className="inline-block bg-blue-600 rounded-full p-2 mb-2 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:shadow-lg focus:shadow-lg focus:outline-none focus:ring-0 active:shadow-lg transition duration-150 ease-in-out"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 320 512"
+                className="w-4 h-4"
+              >
+                <path
+                  fill="currentColor"
+                  d="M279.14 288l14.22-92.66h-88.91v-60.13c0-25.35 12.42-50.06 52.24-50.06h40.42V6.26S260.43 0 225.36 0c-73.22 0-121.08 44.38-121.08 124.72v70.62H22.89V288h81.39v224h100.17V288z"
+                />
+              </svg>
+            </button>
           </div>
         </div>
         <span className="text-[10px] mt-2 text-gray-400 tracking-wide w-[60%] ">
