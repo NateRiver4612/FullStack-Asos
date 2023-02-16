a0523cf69dbaf56b1f542a0be02d74f7e2f3a8be
NateRiver4612.github.io <nguyenchinhan502@gmail.com>
2023-02-17 00:55:27 +0700

    Fix cors problem

--- utils/cors.utils.js
+++ utils/cors.utils.js
@@ -1,6 +1,9 @@
 const allowCors = (fn) => async (req, res) => {
   res.setHeader("Access-Control-Allow-Credentials", true);
-  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Origin",
+    "http://localhost:3000/api/graphql"
+  );
   // another common pattern
   res.setHeader("Access-Control-Allow-Origin", req.headers.origin);
   res.setHeader(
