82c32e19ff74b9a8572df4a8517c5b9d5ec22577
NateRiver4612.github.io <nguyenchinhan502@gmail.com>
2023-02-17 01:54:58 +0700

    Fix cors problem

--- utils/apolloClient.js
+++ utils/apolloClient.js
@@ -1,12 +1,12 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 const client = new ApolloClient({
-  uri: "http://localhost:3000/api/graphql",
+  uri: "http://localhost:3000/api/graphql/",
   cache: new InMemoryCache(),
   headers: {
     Authorization: process.env.NEXT_PUBLIC_WP_AUTHORIZATION,
     "Content-Type": "application/json",
-    "Access-Control-Allow-Origin": "http://localhost:3000/api/grapqh",
+    "Access-Control-Allow-Origin": "http://localhost:3000/api/grapqh/",
     "Access-Control-Allow-Credentials": true,
   },
   credentials: "include",
