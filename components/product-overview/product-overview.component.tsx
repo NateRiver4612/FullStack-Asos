0411c3450ffebd0eb3eb86098fe07a077687deaa
NateRiver4612.github.io <nguyenchinhan502@gmail.com>
2023-02-16 23:39:53 +0700

    Update css of like product animation

--- components/product-overview/product-overview.component.tsx
+++ components/product-overview/product-overview.component.tsx
@@ -46,7 +46,12 @@ const ProductOverview = ({ product }) => {
 
   useEffect(() => {
     const isLiked =
-      likedProducts && likedProducts.find((product) => product.id == id);
+      likedProducts &&
+      likedProducts.find(
+        (product) =>
+          product.id == id &&
+          product.likes.find((like) => like.id == authUser?.id)
+      );
 
     if (isLiked) {
       return setIsProductLiked(true);
@@ -118,11 +123,24 @@ const ProductOverview = ({ product }) => {
           } bg-black/5 rounded-full p-[6px] mb-[10px] mr-[10px]`}
         >
           <motion.div
-            className="box"
+            className="box "
             whileTap={{ scale: 0.4 }}
             transition={{ type: "spring", stiffness: 300, damping: 8 }}
           >
-            {isProductLiked || isclicked ? <AiFillHeart /> : <AiOutlineHeart />}
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill={isProductLiked ? "true" : "none"}
+              viewBox="0 0 24 24"
+              stroke-width="1.7"
+              stroke="currentColor"
+              className="w-6 h-6"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z"
+              />
+            </svg>
           </motion.div>
         </span>
       </div>
